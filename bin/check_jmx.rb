ce4b21ecaf543e37d9568f165ef4073e72ae00fb
Matthias Nuessler <m.nuessler@web.de>
2011-08-28 00:13:54 +0200

    Execute check only if run as main script.

--- bin/check_jmx.rb
+++ bin/check_jmx.rb
@@ -1,83 +1,114 @@
 #!/usr/bin/env jruby
 require 'java'
 require 'rubygems'
-require 'jmx4r' # jruby -S gem install jmx4r
+require 'jmx4r' # install with: jruby -S gem install jmx4r
 require 'optparse'
 require 'pp'
 
-retVals = {
-  :ok => 0,
-  :warning => 1,
-  :critical => 2,
-  :unknown => 3
-}
+module NagiosJMX
+  OK = 0
+  WARNING = 1
+  CRITICAL = 2
+  UNKNOWN = 3
 
-options = {}
-optparse = OptionParser.new do|opts|
-  opts.on('-c', '--critical threshold', Integer, 'Critical threshold') do |c|
-    options[:critical] = c
+  def NagiosJMX.parse_options
+    options = {}
+    optparse = OptionParser.new do|opts|
+      opts.on('-c', '--critical threshold', Integer, 'Critical threshold') do |c|
+        options[:critical] = c
+      end
+
+      opts.on('-w', '--warning threshold', Integer, 'Warning threshold') do |w|
+        options[:warning] = w
+      end
+
+      opts.on('-H', '--host HOST', 'MBean server hostname') do |h|
+        options[:host] = h
+      end
+
+      opts.on('-P', '--port PORT', 'MBean server port') do |p|
+        options[:port] = p
+      end
+
+      opts.on('-b', '--bean BEAN', 'Name of the MBean to query') do |b|
+        options[:mbean] = b
+      end
+
+      opts.on('-a', '--attribute ATTR', 'MBean attribute or operation to query') do |a|
+        options[:attribute] = a
+      end
+
+      opts.on('-u', '--username USER', 'User name for authentication  on MBean server') do |u|
+        options[:username] = u
+      end
+
+      opts.on('-p', '--password PASS', 'Password for authentication on MBean server')  do |p|
+        options[:password] = p
+      end
+
+      opts.on('-h', '--help', 'Display this screen') do
+        puts opts
+        exit UNKNOWN
+      end
+    end
+
+    begin
+      optparse.parse!
+      mandatory = [:critical, :warning, :host, :port, :mbean, :attribute]
+      missing = mandatory.select{ |param| options[param].nil? }
+      if not missing.empty?
+        #        puts "Missing options: #{missing.join(', ')}"
+        puts optparse
+        exit UNKNOWN
+      end                                                                                                                                          
+    rescue OptionParser::InvalidOption, OptionParser::MissingArgument => msg
+      puts msg
+      puts optparse
+      exit UNKNOWN
+    end
+    
+    options
   end
 
-  opts.on('-w', '--warning threshold', Integer, 'Warning threshold') do |w|
-    options[:warning] = w
+  def NagiosJMX.check(options = {})
+    begin
+      con_params = { :host => options[:host], :port => options[:port] }
+      if [:username, :password].any? { |key| options.key?(key)  }
+        con_params.merge(:username => options[:username], :password => options[:password])
+      end
+      
+      JMX::MBean.establish_connection(con_params)
+      mbean = JMX::MBean.find_by_name(options[:mbean])
+      value = mbean.send(options[:attribute])
+      puts value
+  
+      if value >= options[:critical]
+        exit CRITICAL
+      elsif value >= options[:warning]
+        exit WARNING
+      else
+        exit OK
+      end
+  
+    rescue NoMethodError
+      puts "No such attribute '#{options[:attribute]}' for MBean '#{options[:mbean]}'"
+      mbean.attributes.each do |a|
+        puts a
+      end
+      exit UNKNOWN
+    rescue => msg
+      puts msg
+      exit UNKNOWN
+    end
   end
 
-  opts.on('-H', '--host HOST', 'MBean server hostname') do |h|
-    options[:host] = h
-  end
-
-  opts.on('-P', '--port PORT', 'MBean server port') do |p|
-    options[:port] = p
-  end
-
-  opts.on('-b', '--bean BEAN', 'Name of the MBean to query') do |b|
-    options[:bean] = b
-  end
-
-  opts.on('-a', '--attribute ATTR', 'MBean attribute or operation to query') do |a|
-    options[:attribute] = a
-  end
-
-  opts.on('-u', '--user USER', 'User name for authentication  on MBean server') do |u|
-    options[:user] = u
-  end
-
-  opts.on('-p', '--password PASS', 'Password for authentication on MBean server')  do |p|
-  end
-
-  # This displays the help screen, all programs are
-  # assumed to have this option.
-  opts.on('-h', '--help', 'Display this screen') do
-    puts opts
-    exit retVals[:unknown]
-  end
 end
 
-optparse.parse!
+if $0 == __FILE__
+  options = NagiosJMX.parse_options
 
-pp "Options:", options
-pp "ARGV:", ARGV
+  pp "Options:", options
+  pp "ARGV:", ARGV
 
-options.has_key? :host or exit retVals[:unknown]
-
-begin
-  JMX::MBean.establish_connection(:host => options[:host], :port => options[:port])
-  bean = JMX::MBean.find_by_name options[:bean]
-  value = bean.send(options[:attribute])
-  puts value
-  
-  if value >= options[:critical]
-    exit retVals[:critical]
-  elsif value >= options[:warning]
-    exit retVals[:warning]
-  else
-    exit retVals[:ok]
-  end
-  
-rescue NoMethodError
-  puts "No such attribute #{options[:attribute]} for MBean #{options[:bean]}"
-  exit retVals[:unknown]
-rescue => msg
-  puts msg
-  exit retVals[:unknown]
+  NagiosJMX.check(options)
 end
